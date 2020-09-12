17623a4faee5bc065bf8f060071048ac421007da
kenny0619 <kehindeibukun0619@gmail.com>
2020-09-12 20:23:24 +0100

    created method on model to generate a JWT

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const secret = require("../config").secret;
 
 const UserSchema = new mongoose.Schema(
   {
@@ -32,11 +34,33 @@ const UserSchema = new mongoose.Schema(
 
 UserSchema.plugin(uniqueValidator, { message: "is already taken." });
 
-UserSchema.methods.setPassword = () => {
+UserSchema.methods.setPassword = (password) => {
   this.salt = crypto.randomBytes(16).toString("hex");
   this.hash = crypto
     .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
     .toString("hex");
+};
+
+UserSchema.methods.validPassword = (password) => {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+    .toString("hex");
+  return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = () => {
+  const today = new Date();
+  const exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      exp: parseInt(exp.getTime() / 1000),
+    },
+    secret
+  );
 };
 
 mongoose.model("User", UserSchema);
